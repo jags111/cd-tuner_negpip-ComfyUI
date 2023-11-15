d24cdccc7be50be38b9bc101be3c937f2aa53a51
VALADI K JAGANATHAN <info@orchidsasia.com>
2023-11-15 21:32:45 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,9 @@
+#
+import os, shutil
+import folder_paths
+
+module_js_directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), "js")
+application_root_directory = os.path.dirname(folder_paths.__file__)
 from .cd_tuner import CDTuner
 from .negpip import Negpip
 
